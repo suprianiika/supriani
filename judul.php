3781fcd88ca1a770da6ef4c4013345c6ec774c6c
suprianiika <128717313+suprianiika@users.noreply.github.com>
2023-03-26 15:58:13 +0800

    Add files via upload

---
+++ judul.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+  </head>
+  <body>
+  <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Film</title>
+</head>
+<body>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap4.min.css">
+<p><a href="#" class="text-primary">
+<div class="container">
+<h1>Web Pencarian Film</h1>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+		<form method="post" action="index.php">
+        <input type="text" name="judul"></input>
+        <input type="submit" value="Pencarian" name="cari"></input>
+    </form>
+    </a></p>
+    <?php
+        if(isset($_POST['cari'])){
+            $judul = $_POST['judul'];
+            echo "<h1>Hasil Pencarian</h1>";
+        
+            $url = '1a27b37f4a1f08c046e42999b9337fc4'.$judul.'"';
+
+           //Akses API dengan CURL
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $output = curl_exec($ch);
+            curl_close($ch);
+
+            // var_dump($output);
+            $data = json_decode($output, TRUE);
+            // $data = $data['Search'];
+            
+            foreach ($data['Search'] as $movie); {
+             
+                echo "\n <p>Judul: ".$movie["Title"]."</p>";
+                echo "<p>Tahun: ".$movie["Year"]."</p>";
+                echo '<img src= "'.$movie['Poster'].'">';
+            }    
+			
+		}
+        ?>
+    
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
