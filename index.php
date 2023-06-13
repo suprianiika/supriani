bc7c6d2cd06d4c2aac9b5762d3def23d6b46fddf
suprianiika <128717313+suprianiika@users.noreply.github.com>
2023-06-13 11:32:41 +0800

    Add files via upload
    project pemprograman jaringan

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Onlineshop Baju Toraja</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="header">
+    <p>Onlineshop Baju Toraja  </p>
+  </div>
+  <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+  </nav>
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+  <div class="footer">
+    <p>selamat datang | di Online Shop | Baju Toraja <a href="" target="_blank">Framework Id</a></p>
+  </div>
+
+</body>
+
+</html>
